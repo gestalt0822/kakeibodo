a554a498b169a22f925efe5518464cda6e893f7f
青木大介 <AokiDaisuke@aokidaisuke-no-MacBook-Pro.local>
2017-01-07 18:18:32 +0900

    入力機能を実装

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,4 +2,21 @@ class BookingsController < ApplicationController
   def index
     @bookings = Booking.all
   end
+  #http://www.namaraii.com/rubytips/datetime/#section-7
+
+  def new
+   @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(bookings_params)
+    @booking.date = Date.today
+    @booking.save
+    redirect_to bookings_path
+  end
+
+  private
+    def bookings_params
+      params.require(:booking).permit(:amount, :detail ,:date)
+    end
 end
