85bdbe41eb3f246b47c1af287c9bc9bed9e86faa
青木大介 <AokiDaisuke@aokidaisuke-no-MacBook-Pro.local>
2017-01-09 14:40:04 +0900

    sorts edit,updateアクションを実装

--- app/controllers/sorts_controller.rb
+++ app/controllers/sorts_controller.rb
@@ -12,6 +12,16 @@ class SortsController < ApplicationController
     redirect_to sorts_path
   end
 
+  def edit
+    @sort = Sort.find(params[:id])
+  end
+
+  def update
+    @sort = Sort.find(params[:id])
+    @sort.update(sorts_params)
+    redirect_to sorts_path
+  end
+
   private
    def sorts_params
      params.require(:sort).permit(:name, :category_id)
