91892dbca6a805144acfc2dab2d7a4da43ee97c1
青木大介 <AokiDaisuke@aokidaisuke-no-MacBook-Pro.local>
2017-01-11 21:15:08 +0900

    challengesのルーティング,index,create,newのアクションとビューを作成

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -1,10 +1,22 @@
 class ChallengesController < ApplicationController
   def index
+    @challenges = Challenge.all
   end
 
   def new
+    @challenge = Challenge.new
   end
 
   def create
+    @challenge = Challenge.new(challenges_params)
+    @challenge.user_id = current_user.id
+    @challenge.start = Date.today
+    @challenge.save
+    redirect_to challenges_path
   end
+
+  private
+    def challenges_params
+      params.require(:challenge).permit(:deadline, :target)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 Rails.application.routes.draw do
-  get 'challenges/index'
-
-  get 'challenges/new'
-
-  get 'challenges/create'
-
   root 'bookings#index'
   devise_for :users
   resources :bookings, except: :show
   resources :categories, only:[:index, :new ,:create, :destroy, :edit, :update]
   resources :sorts, except: :show
+  resources :challenges, only:[:index, :new ,:create]
 end
