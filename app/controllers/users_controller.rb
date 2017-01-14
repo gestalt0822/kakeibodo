41ffffef0e130afc1f2c9e063c2d2a6006604ae9
青木大介 <AokiDaisuke@aokidaisuke-no-MacBook-Pro.local>
2017-01-14 22:22:30 +0900

    Rankingモデルを作成&マイページを作成し得点別にランクを表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,8 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
+#毎回、計算していていいのか？
+#challengesコントローラーでチャレンジ終了時にやるべきでは？
   def show
     @user = User.find(params[:id])
     if @user.challenges.where(user_id: @user.id)
@@ -14,6 +16,29 @@ class UsersController < ApplicationController
         end
       end
         @user.update(total_score: total_scores)
+    end
+
+    @your_score = current_user.total_score
+  if Ranking.find(1).threshold > @your_score
+    @ranking = Ranking.find(1).name
+  elsif Ranking.find(2).threshold > @your_score
+    @ranking = Ranking.find(2).name
+  elsif Ranking.find(3).threshold > @your_score
+    @ranking = Ranking.find(3).name
+  elsif Ranking.find(4).threshold > @your_score
+    @ranking = Ranking.find(4).name
+  elsif Ranking.find(5).threshold > @your_score
+    @ranking = Ranking.find(5).name
+  elsif Ranking.find(6).threshold > @your_score
+    @ranking = Ranking.find(6).name
+  elsif Ranking.find(7).threshold > @your_score
+    @ranking = Ranking.find(7).name
+  elsif Ranking.find(8).threshold > @your_score
+    @ranking = Ranking.find(9).name
+  elsif Ranking.find(10).threshold > @your_score
+    @ranking = Ranking.find(10).name
+  else
+    @ranking = "ランク外"
     end
   end
 end
