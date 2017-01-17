4618333c94cc2ede2136403d82a4c355750f76b7
青木大介 <AokiDaisuke@aokidaisuke-no-MacBook-Pro.local>
2017-01-17 20:03:42 +0900

    集計機能に割合を追加

--- app/controllers/analyses_controller.rb
+++ app/controllers/analyses_controller.rb
@@ -3,10 +3,14 @@ class AnalysesController < ApplicationController
    @categories = current_user.bookings.select(:category_id).distinct#該当ユーザの家計簿のcategory_idを重複なしで配列に
    @amounts = Array.new
    num = 0
+
+
+
    while num < @categories.count do
      @each_amount = Array.new
-     @each_amount << Category.find(@categories[num].category_id ).name
+     @each_amount << Category.find(@categories[num].category_id).name
      @each_amount << current_user.bookings.where(category_id: @categories[num].category_id).sum(:amount)
+     @each_amount << (current_user.bookings.where(category_id:@categories[num].category_id).sum(:amount).to_f/current_user.bookings.sum(:amount).to_f*100).round(2)
      @amounts << @each_amount
      num += 1
    end
