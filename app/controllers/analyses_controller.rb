441704f02694b4566d8805624ecfbccbfcb3da94
青木大介 <AokiDaisuke@aokidaisuke-no-MacBook-Pro.local>
2017-01-18 20:30:02 +0900

    集計を今月と先月までに分割

--- app/controllers/analyses_controller.rb
+++ app/controllers/analyses_controller.rb
@@ -1,15 +1,28 @@
 class AnalysesController < ApplicationController
  def index
-   @categories = current_user.bookings.select(:category_id).distinct#該当ユーザの家計簿のcategory_idを重複なしで配列に
-   @amounts = Array.new
+   @categories_now = current_user.bookings.where(date: Time.now.beginning_of_month..Time.now.end_of_month).select(:category_id).distinct#該当ユーザの家計簿のcategory_idを重複なしで配列に
+   @amounts_now = Array.new
    num = 0
 
-   while num < @categories.count do
+   while num < @categories_now.count do
      @each_amount = Array.new
-     @each_amount << Category.find(@categories[num].category_id).name
-     @each_amount << current_user.bookings.where(category_id: @categories[num].category_id).sum(:amount)
-     @each_amount << (current_user.bookings.where(category_id:@categories[num].category_id).sum(:amount).to_f/current_user.bookings.sum(:amount).to_f*100).round(2)
-     @amounts << @each_amount
+     @each_amount << Category.find(@categories_now[num].category_id).name
+     @each_amount << current_user.bookings.where(category_id: @categories_now[num].category_id).sum(:amount)
+     @each_amount << (current_user.bookings.where(category_id:@categories_now[num].category_id).sum(:amount).to_f/current_user.bookings.where(date: Time.now.beginning_of_month..Time.now.end_of_month).sum(:amount).to_f*100).round(2)
+     @amounts_now << @each_amount
+     num += 1
+   end
+
+   @categories_then = current_user.bookings.where(date: Time.now.prev_month.beginning_of_month..Time.now.prev_month.end_of_month).select(:category_id).distinct#該当ユーザの家計簿のcategory_idを重複なしで配列に
+   @amounts_then = Array.new
+   num = 0
+
+   while num < @categories_then.count do
+     @each_amount = Array.new
+     @each_amount << Category.find(@categories_then[num].category_id).name
+     @each_amount << current_user.bookings.where(category_id: @categories_then[num].category_id).sum(:amount)
+     @each_amount << (current_user.bookings.where(category_id:@categories_then[num].category_id).sum(:amount).to_f/current_user.bookings.where(date: Time.now.prev_month.beginning_of_month..Time.now.prev_month.end_of_month).sum(:amount).to_f*100).round(2)
+     @amounts_then << @each_amount
      num += 1
    end
  end
