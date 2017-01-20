46884ad0660e1d4af93a576c51e16d83ad99e5af
青木大介 <AokiDaisuke@aokidaisuke-no-MacBook-Pro.local>
2017-01-20 16:10:20 +0900

    消去したbookingsの項目は集計でも対象外に

--- app/controllers/analyses_controller.rb
+++ app/controllers/analyses_controller.rb
@@ -1,27 +1,27 @@
 class AnalysesController < ApplicationController
  def index
-   @categories_now = current_user.bookings.where(date: Time.now.beginning_of_month..Time.now.end_of_month).select(:category_id).distinct#該当ユーザの家計簿のcategory_idを重複なしで配列に
+   @categories_now = current_user.bookings.where.not(unlist: true).where(date: Time.now.beginning_of_month..Time.now.end_of_month).select(:category_id).distinct#該当ユーザの家計簿のcategory_idを重複なしで配列に
    @amounts_now = Array.new
    num = 0
 
    while num < @categories_now.count do
      @each_amount = Array.new
      @each_amount << Category.find(@categories_now[num].category_id).name#カテゴリー名
-     @each_amount << current_user.bookings.where(category_id: @categories_now[num].category_id).sum(:amount)#金額
-     @each_amount << (current_user.bookings.where(category_id:@categories_now[num].category_id).where(date: Time.now.beginning_of_month..Time.now.end_of_month).sum(:amount).to_f/current_user.bookings.where(date: Time.now.beginning_of_month..Time.now.end_of_month).sum(:amount).to_f*100).round(1)
+     @each_amount << current_user.bookings.where.not(unlist: true).where(category_id: @categories_now[num].category_id).sum(:amount)#金額
+     @each_amount << (current_user.bookings.where(category_id:@categories_now[num].category_id).where.not(unlist: true).where(date: Time.now.beginning_of_month..Time.now.end_of_month).sum(:amount).to_f/current_user.bookings.where.not(unlist: true).where(date: Time.now.beginning_of_month..Time.now.end_of_month).sum(:amount).to_f*100).round(1)
      @amounts_now << @each_amount
      num += 1
    end
 
-   @categories_then = current_user.bookings.where.not(date: Time.now.beginning_of_month..Time.now.end_of_month).select(:category_id).distinct#該当ユーザの家計簿のcategory_idを重複なしで配列に
+   @categories_then = current_user.bookings.where.not(unlist: true).where.not(unlist: true).where.not(date: Time.now.beginning_of_month..Time.now.end_of_month).select(:category_id).distinct#該当ユーザの家計簿のcategory_idを重複なしで配列に
    @amounts_then = Array.new
    num = 0
 
    while num < @categories_then.count do
      @each_amount = Array.new
      @each_amount << Category.find(@categories_then[num].category_id).name
-     @each_amount << current_user.bookings.where(category_id: @categories_then[num].category_id).sum(:amount)
-     @each_amount << (current_user.bookings.where(category_id:@categories_then[num].category_id).where.not(date: Time.now.beginning_of_month..Time.now.end_of_month).sum(:amount).to_f/current_user.bookings.where.not(date: Time.now.beginning_of_month..Time.now.end_of_month).sum(:amount).to_f*100).round(1)
+     @each_amount << current_user.bookings.where.not(unlist: true).where(category_id: @categories_then[num].category_id).sum(:amount)
+     @each_amount << (current_user.bookings.where(category_id:@categories_then[num].category_id).where.not(unlist: true).where.not(date: Time.now.beginning_of_month..Time.now.end_of_month).sum(:amount).to_f/current_user.bookings.where.not(unlist: true).where.not(date: Time.now.beginning_of_month..Time.now.end_of_month).sum(:amount).to_f*100).round(1)
      @amounts_then << @each_amount
      num += 1
    end
