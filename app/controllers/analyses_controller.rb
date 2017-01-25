2b60daface10d856af6c29817f3e8b8b4e126fa3
青木大介 <AokiDaisuke@aokidaisuke-no-MacBook-Pro.local>
2017-01-25 19:43:19 +0900

    analysis_controllerをリファクタ(scopeを使用)

--- app/controllers/analyses_controller.rb
+++ app/controllers/analyses_controller.rb
@@ -1,32 +1,43 @@
 class AnalysesController < ApplicationController
  def index
-   @challenge_now = Challenge.where(user_id: current_user.id, status:1)
-   @categories_now = current_user.bookings.where.not(unlist: true).where(date: Time.now.beginning_of_month..Time.now.end_of_month).select(:category_id).distinct#該当ユーザの家計簿のcategory_idを重複なしで配列に
-   @amounts_now = Array.new
-   num = 0
+   @categories_now = current_user.bookings.where(unlist: false).this_month.select(:category_id).distinct
+   #該当ユーザの家計簿のcategory_idを重複なしで配列に
 
-   while num < @categories_now.count do
-     @each_amount = Array.new
-     @each_amount << Category.find(@categories_now[num].category_id).name#カテゴリー名
-     @each_amount << current_user.bookings.where.not(unlist: true).where(category_id: @categories_now[num].category_id).sum(:amount)#金額
-     @each_amount << (current_user.bookings.where(category_id:@categories_now[num].category_id).where.not(unlist: true).where(date: Time.now.beginning_of_month..Time.now.end_of_month).sum(:amount).to_f/current_user.bookings.where.not(unlist: true).where(date: Time.now.beginning_of_month..Time.now.end_of_month).sum(:amount).to_f*100).round(1)
-     @amounts_now << @each_amount
-     num += 1
+   def amounts_now(categories)
+     amounts_now = Array.new
+     num = 0
+     while num < categories.count do
+       each_amount = Array.new
+       each_amount << Category.find(categories[num].category_id).name#カテゴリー名
+       each_amount << current_user.bookings.where(category_id: categories[num].category_id, unlist:false).sum(:amount)#金額
+       each_amount << (current_user.bookings.where(category_id:categories[num].category_id, unlist:false).this_month.sum(:amount).to_f/current_user.bookings.where.not(unlist: true).this_month.sum(:amount).to_f*100).round(1)
+       amounts_now << each_amount
+       num += 1
+     end
+     return amounts_now
+   end
+   @amounts_now = amounts_now(@categories_now)
+   #this_monthはbookings.rbを参照
+
+   @categories_then = current_user.bookings.where(unlist: false).except_this_month.select(:category_id).distinct
+   #該当ユーザの家計簿のcategory_idを重複なしで配列に
+
+   def amounts_then(categories)
+     amounts_then = Array.new
+     num = 0
+
+     while num < categories.count do
+       each_amount = Array.new
+       each_amount << Category.find(categories[num].category_id).name
+       each_amount << current_user.bookings.where(category_id: categories[num].category_id, unlist:false).sum(:amount)
+       each_amount << (current_user.bookings.where(category_id: categories[num].category_id, unlist:false).except_this_month.sum(:amount).to_f/current_user.bookings.where(unlist: false).except_this_month.sum(:amount).to_f*100).round(1)
+       amounts_then << each_amount
+       num += 1
+     end
+     return amounts_then
    end
 
-   @categories_then = current_user.bookings.where.not(unlist: true).where.not(unlist: true).where.not(date: Time.now.beginning_of_month..Time.now.end_of_month).select(:category_id).distinct#該当ユーザの家計簿のcategory_idを重複なしで配列に
-   @amounts_then = Array.new
-   num = 0
-
-   while num < @categories_then.count do
-     @each_amount = Array.new
-     @each_amount << Category.find(@categories_then[num].category_id).name
-     @each_amount << current_user.bookings.where.not(unlist: true).where(category_id: @categories_then[num].category_id).sum(:amount)
-     @each_amount << (current_user.bookings.where(category_id:@categories_then[num].category_id).where.not(unlist: true).where.not(date: Time.now.beginning_of_month..Time.now.end_of_month).sum(:amount).to_f/current_user.bookings.where.not(unlist: true).where.not(date: Time.now.beginning_of_month..Time.now.end_of_month).sum(:amount).to_f*100).round(1)
-     @amounts_then << @each_amount
-     num += 1
-   end
- end
-
-
+   @amounts_then = amounts_then(@categories_then)
+   #except_this_monthはbookings.rbを参照
+  end
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,4 +3,6 @@ class Booking < ActiveRecord::Base
   belongs_to :category
   belongs_to :sort
   has_many :booking_lists, foreign_key: "booking_id", class_name: "Bookandchallenge"
+  scope :except_this_month, -> {where.not(date: Time.now.beginning_of_month..Time.now.end_of_month)}
+  scope :this_month, -> {where(date: Time.now.beginning_of_month..Time.now.end_of_month)}
 end
