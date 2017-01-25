7b4f8af91a360c2a13247bf2483ce47bc1f0fa64
青木大介 <AokiDaisuke@aokidaisuke-no-MacBook-Pro.local>
2017-01-25 15:36:56 +0900

    challenges_controllerをリファクタ(show)

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -29,17 +29,8 @@ class ChallengesController < ApplicationController
 
   def show
     @challenges = @challenge.challenge_lists
-
-    #基本ポイントは挑戦日数
-    if @challenge.deadline.yday < @challenge.start.yday#年をまたぐ場合、チャレンジ期間に矛盾が生じないようにするための処理
-      @duration_point = (@challenge.deadline.yday + 365) - @challenge.start.yday
-    else
-      @duration_point = @challenge.deadline.yday - @challenge.start.yday#チャレンジ期間を判定
-    end
-    @amount_point = @challenge.total_amount.to_f/@challenge.target.to_f#パーセントを小数点で表す(例).10%なら0.1
-    @amount_point *= 100
-    @amount_point = 100 - @amount_point
-    @amount_point /= 100#余裕率を小数点で表す(10%なら0.1)
+    @duration_point = @challenge.calculate_duration_point#挑戦日数を計算(challenge.rb)
+    @amount_point = @challenge.calculate_amount_point#余裕率を計算(challenge.rb)
     #基本ポイントX余裕率のボーナス(例).基本ポイント30で余裕率10%なら33ポイント
     @amount_bonus = @duration_point * @amount_point #期間のボーナス、
     @total_score = @duration_point + @amount_bonus
--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -2,8 +2,20 @@ class Challenge < ActiveRecord::Base
   belongs_to :user
   has_many :challenge_lists, foreign_key: "challenge_id", class_name: "Bookandchallenge"
   has_many :moneys, through: :challenge_lists, source: :booking#challengeから直接bookingsを取得
-
   enum achieve: {目標達成:true, 目標未達:false}
 
-  
+  def calculate_amount_point
+    amount_point = self.total_amount.to_f/self.target.to_f#パーセントを小数点で表す(例).10%なら0.1
+    amount_point *= 100
+    amount_point = 100 - amount_point
+    amount_point /= 100#余裕率を小数点で表す(10%なら0.1)
+  end
+
+  def calculate_duration_point
+    if self.deadline.yday < self.start.yday#年をまたぐ場合、チャレンジ期間に矛盾が生じないようにするための処理
+      duration_point = (self.deadline.yday + 365) - self.start.yday
+    else
+      duration_point = self.deadline.yday - self.start.yday#チャレンジ期間を判定
+    end
+  end
 end
