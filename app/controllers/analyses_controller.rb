836072a1ac3111539d23d434871574947ef46970
青木大介 <AokiDaisuke@aokidaisuke-no-MacBook-Pro.local>
2017-01-25 20:00:01 +0900

    analysis_controllerをリファクタ(scopeを使用,listed)

--- app/controllers/analyses_controller.rb
+++ app/controllers/analyses_controller.rb
@@ -1,6 +1,6 @@
 class AnalysesController < ApplicationController
  def index
-   @categories_now = current_user.bookings.where(unlist: false).this_month.select(:category_id).distinct
+   @categories_now = current_user.bookings.listed.this_month.select(:category_id).distinct
    #該当ユーザの家計簿のcategory_idを重複なしで配列に
 
    def amounts_now(categories)
@@ -9,17 +9,16 @@ class AnalysesController < ApplicationController
      while num < categories.count do
        each_amount = Array.new
        each_amount << Category.find(categories[num].category_id).name#カテゴリー名
-       each_amount << current_user.bookings.where(category_id: categories[num].category_id, unlist:false).sum(:amount)#金額
-       each_amount << (current_user.bookings.where(category_id:categories[num].category_id, unlist:false).this_month.sum(:amount).to_f/current_user.bookings.where.not(unlist: true).this_month.sum(:amount).to_f*100).round(1)
+       each_amount << current_user.bookings.listed.where(category_id:categories[num].category_id).sum(:amount)#金額
+       each_amount << (current_user.bookings.listed.where(category_id:categories[num].category_id).this_month.sum(:amount).to_f/current_user.bookings.where(unlist:false).this_month.sum(:amount).to_f*100).round(1)
        amounts_now << each_amount
        num += 1
      end
      return amounts_now
    end
    @amounts_now = amounts_now(@categories_now)
-   #this_monthはbookings.rbを参照
 
-   @categories_then = current_user.bookings.where(unlist: false).except_this_month.select(:category_id).distinct
+   @categories_then = current_user.bookings.listed.except_this_month.select(:category_id).distinct
    #該当ユーザの家計簿のcategory_idを重複なしで配列に
 
    def amounts_then(categories)
@@ -29,8 +28,8 @@ class AnalysesController < ApplicationController
      while num < categories.count do
        each_amount = Array.new
        each_amount << Category.find(categories[num].category_id).name
-       each_amount << current_user.bookings.where(category_id: categories[num].category_id, unlist:false).sum(:amount)
-       each_amount << (current_user.bookings.where(category_id: categories[num].category_id, unlist:false).except_this_month.sum(:amount).to_f/current_user.bookings.where(unlist: false).except_this_month.sum(:amount).to_f*100).round(1)
+       each_amount << current_user.bookings.listed.where(category_id: categories[num].category_id).sum(:amount)
+       each_amount << (current_user.bookings.listed.where(category_id: categories[num].category_id).except_this_month.sum(:amount).to_f/current_user.bookings.where(unlist: false).except_this_month.sum(:amount).to_f*100).round(1)
        amounts_then << each_amount
        num += 1
      end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -5,4 +5,5 @@ class Booking < ActiveRecord::Base
   has_many :booking_lists, foreign_key: "booking_id", class_name: "Bookandchallenge"
   scope :except_this_month, -> {where.not(date: Time.now.beginning_of_month..Time.now.end_of_month)}
   scope :this_month, -> {where(date: Time.now.beginning_of_month..Time.now.end_of_month)}
+  scope :listed, -> {where(unlist:false)}
 end
