b99e9c364446c389da4d845c9bc87383b3dfdea9
青木大介 <AokiDaisuke@aokidaisuke-no-MacBook-Pro.local>
2017-01-25 20:59:53 +0900

    users_controllerをリファクタ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,61 +11,22 @@ class UsersController < ApplicationController
     end
 
     #合計スコアによって当てはまるランクを表示
-    if Ranking.find(1).threshold > @your_score
-      @ranking = Ranking.find(1).name
-      @next_rank = Ranking.find(2).name
-      @score_difference = Ranking.find(1).threshold - @your_score
-      @message = Message.find(1).content
-      @messenger = Message.find(1).messenger
-    elsif Ranking.find(2).threshold > @your_score
-      @ranking = Ranking.find(2).name
-      @next_rank = Ranking.find(3).name
-      @score_difference = Ranking.find(2).threshold - @your_score
-      @message = Message.find(2).content
-      @messenger = Message.find(2).messenger
-    elsif Ranking.find(3).threshold > @your_score
-      @ranking = Ranking.find(3).name
-      @next_rank = Ranking.find(4).name
-      @score_difference = Ranking.find(3).threshold - @your_score
-      @message = Message.find(3).content
-      @messenger = Message.find(3).messenger
-    elsif Ranking.find(4).threshold > @your_score
-      @ranking = Ranking.find(4).name
-      @next_rank = Ranking.find(5).name
-      @score_difference = Ranking.find(4).threshold - @your_score
-      @message = Message.find(4).content
-      @messenger = Message.find(4).messenger
-    elsif Ranking.find(5).threshold > @your_score
-      @ranking = Ranking.find(5).name
-      @next_rank = Ranking.find(6).name
-      @score_difference = Ranking.find(5).threshold - @your_score
-      @message = Message.find(5).content
-      @messenger = Message.find(5).messenger
-    elsif Ranking.find(6).threshold > @your_score
-      @ranking = Ranking.find(6).name
-      @next_rank = Ranking.find(7).name
-      @score_difference = Ranking.find(6).threshold - @your_score
-      @message = Message.find(6).content
-      @messenger = Message.find(6).messenger
-    elsif Ranking.find(7).threshold > @your_score
-      @ranking = Ranking.find(7).name
-      @next_rank = Ranking.find(8).name
-      @score_difference = Ranking.find(7).threshold - @your_score
-      @message = Message.find(7).content
-      @messenger = Message.find(7).messenger
-    elsif Ranking.find(8).threshold > @your_score
-      @ranking = Ranking.find(8).name
-      @next_rank = Ranking.find(9).name
-      @score_difference = Ranking.find(8).threshold - @your_score
-      @message = Message.find(8).content
-      @messenger = Message.find(8).messenger
-    elsif Ranking.find(9).threshold > @your_score
-      @ranking = Ranking.find(9).name
-      @next_rank = Ranking.find(10).name
-      @score_difference = Ranking.find(9).threshold - @your_score
-      @message = Message.find(9).content
-      @messenger = Message.find(9).messenger
-    else
+    i = 1
+
+    while i < 10 do
+      if Ranking.find(i).threshold > @your_score
+        @ranking = Ranking.find(i).name
+        @next_rank = Ranking.find(i+1).name
+        @score_difference = Ranking.find(i).threshold - @your_score
+        @message = Message.find(i).content
+        @messenger = Message.find(i).messenger
+        break
+      else
+        i = i +1
+      end
+    end
+
+    if Ranking.find(10).threshold <= @your_score
       @ranking = "ランク外"
       @next_rank = "ありません、あなたは最強だから"
       @score_difference = "0"
