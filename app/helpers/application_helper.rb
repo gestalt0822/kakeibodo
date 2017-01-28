7d148c30aa8ac9621f76ad1a17c2b8ac91d77fb3
青木大介 <AokiDaisuke@aokidaisuke-no-MacBook-Pro.local>
2017-01-28 23:44:39 +0900

    ランキング関連のviewの処理をhelperに

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,4 +13,50 @@ module ApplicationHelper
     return true
     end
   end
+
+  def which_user?(index)
+    if index == 1
+      @user_rank = "gold"
+      return  @user_rank
+    elsif index == 2
+      @user_rank = "silver"
+      return @user_rank
+    elsif index == 3
+      @user_rank = "bronze"
+      return @user_rank
+    else
+      @user_rank= "each"
+      return @user_rank
+    end
+  end
+
+  def which_medal?(index)
+    if index == 1
+      return image_tag('0173_gold.png', size: '130x130', class:'img-responsive medal')
+    elsif index == 2
+      return image_tag('0172_silver.png', size: '130x130', class:'img-responsive medal')
+    elsif index == 3
+      return image_tag('0171_bronze.png', size: '130x130', class:'img-responsive medal')
+    else
+    end
+  end
+
+  def which_rank?(index)
+    if index == 1
+      return  1
+    elsif index == 2
+      return 2
+    elsif index == 3
+      return 3
+    else
+    end
+  end
+
+  def user_image(user)
+    if user.avatar.present?
+      return image_tag(user.avatar, size: '130x130', class:'img-responsive user_image')
+    else
+      return image_tag('no_image.png', size: '130x130', class:'img-responsive user_image')
+    end
+  end
 end
